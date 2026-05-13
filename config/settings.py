05baeae8fb3ba827b8a6bb74bc47d0fa053ee5f6
Saulo Titonel Bastos <saulo@ame-caraguatatuba.local>
2026-05-13 17:14:18 +0000

    fix: adiciona STATIC_ROOT e MEDIA_ROOT ao settings.py
    STATIC_ROOT = BASE_DIR / 'staticfiles'
      Necessário para que collectstatic saiba onde gravar os arquivos.
    
    MEDIA_URL / MEDIA_ROOT = BASE_DIR / 'media'
      Formaliza o caminho para uploads de contratos PDF e arquivos SIRESP,
      que já eram gravados em media/ por convenção.

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,11 @@ TIME_ZONE = 'America/Sao_Paulo'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL  = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL  = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
